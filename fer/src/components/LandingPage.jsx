1e1010719aa486d23e962b6b72dc853413b1f1e3
CarolDsillva <caroldsillva@yahoo.com>
2024-12-29 13:34:59 +0530

    loading screen

--- fer/src/components/LandingPage.jsx
+++ fer/src/components/LandingPage.jsx
@@ -5,6 +5,7 @@ import "./LandingPage.css";
 const LandingPage = () => {
     const [selectedImage, setSelectedImage] = useState(null);
     const [confirmationMessage, setConfirmationMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false); // New state for loading
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
     const [isCameraOpen, setIsCameraOpen] = useState(false);
@@ -50,6 +51,8 @@ const LandingPage = () => {
             return;
         }
 
+        setIsLoading(true); // Start loading
+
         const formData = new FormData();
         formData.append("file", selectedImage);
 
@@ -68,17 +71,24 @@ const LandingPage = () => {
             navigate("/music-player", { state: { emotion: data.emotion, confidence: data.confidence, recommendedSongs: data.recommended_songs } });
         } catch (error) {
             console.error("Error detecting emotion:", error);
+        } finally {
+            setIsLoading(false); // Stop loading
         }
     };
 
     return (
         <div className="landing-container">
-    
+            {isLoading && (
+                <div className="loading-overlay">
+                    <div className="loading-spinner"></div>
+                    <p>Predicting emotion</p>
+                </div>
+            )}
             <h2 className="logo-name">🎵 emuziek</h2>
             <p className="top-label">👾 ML Emotion Music Recommender</p>
-            <h1 className="hero-text">Discover Music <br/> That Matches Your Mood</h1>
+            <h1 className="hero-text">Discover Music <br /> That Matches Your Mood</h1>
             <p className="sub-text">
-                Analyze your emotions, recommend songs and deliver personalized playlists crafted just for you. <br/> Experience music that understands you.
+                Analyze your emotions, recommend songs and deliver personalized playlists crafted just for you. <br /> Experience music that understands you.
             </p>
             <div className="upload-container">
                 <input type="file" id="image-upload" onChange={handleImageUpload} />
